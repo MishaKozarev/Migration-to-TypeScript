500d0df7f01392fd5cf8d8e8af310f8893e8c7ab
MishaKozarev <mishaaaaa1986@mail.ru>
2023-06-09 15:27:43 +0300

    feat: add apiKey in appLoader

--- migration-to-ts/src/components/controller/appLoader.js
+++ migration-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'cf436ac028804f3e8cb88556155d0522', // получите свой ключ https://newsapi.org/
         });
     }
 }
