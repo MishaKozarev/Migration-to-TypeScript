06ce20485424395af785109c18cb5a4b06b07a34
MishaKozarev <mishaaaaa1986@mail.ru>
2023-06-10 22:13:26 +0300

    feat: add interface NewsResponse and change class News

---
+++ migration-to-ts/src/components/view/news/news.ts
@@ -0,0 +1,68 @@
+import './news.css';
+import { NewsResponse } from '../../../types/index';
+
+class News {
+    draw(data: NewsResponse[]): void {
+        const news: NewsResponse[] =
+            data.length >= 10 ? data.filter((_item: NewsResponse, idx: number) => idx < 10) : data;
+
+        const fragment: DocumentFragment = document.createDocumentFragment();
+        const newsItemTemp: HTMLTemplateElement | null = document.querySelector('#newsItemTemp');
+
+        if (newsItemTemp) {
+            news.forEach((item: NewsResponse, idx: number) => {
+                const newsClone: DocumentFragment = newsItemTemp.content.cloneNode(true) as DocumentFragment;
+                if (idx % 2) {
+                    const newsCloneItem: HTMLElement | null = newsClone.querySelector('.news__item');
+                    if (newsCloneItem) {
+                        newsCloneItem.classList.add('alt');
+                    }
+                }
+
+                const newsMetaPhoto: HTMLElement | null = newsClone.querySelector('.news__meta-photo');
+                if (newsMetaPhoto) {
+                    newsMetaPhoto.style.backgroundImage = `url(${item.urlToImage || 'img/news_placeholder.jpg'})`;
+                }
+
+                const newsMetaAuthor: HTMLElement | null = newsClone.querySelector('.news__meta-author');
+                if (newsMetaAuthor) {
+                    newsMetaAuthor.textContent = item.author || item.source.name;
+                }
+
+                const newsMetaDate: HTMLElement | null = newsClone.querySelector('.news__meta-date');
+                if (newsMetaDate) {
+                    newsMetaDate.textContent = item.publishedAt.slice(0, 10).split('-').reverse().join('-');
+                }
+
+                const newsDescriptionTitle: HTMLElement | null = newsClone.querySelector('.news__description-title');
+                if (newsDescriptionTitle) {
+                    newsDescriptionTitle.textContent = item.title;
+                }
+
+                const newsDescriptionSource: HTMLElement | null = newsClone.querySelector('.news__description-source');
+                if (newsDescriptionSource) {
+                    newsDescriptionSource.textContent = item.source.name;
+                }
+
+                const newsDescriptionСontent: HTMLElement | null =
+                    newsClone.querySelector('.news__description-content');
+                if (newsDescriptionСontent) {
+                    newsDescriptionСontent.textContent = item.description;
+                }
+
+                const newsReadMore: HTMLElement | null = newsClone.querySelector('.news__read-more a');
+                if (newsReadMore) {
+                    newsReadMore.setAttribute('href', item.url);
+                }
+                fragment.append(newsClone);
+            });
+        }
+        const newsWrapper: HTMLElement | null = document.querySelector('.news');
+        if (newsWrapper) {
+            newsWrapper.innerHTML = '';
+            newsWrapper.appendChild(fragment);
+        }
+    }
+}
+
+export default News;
---
+++ migration-to-ts/src/types/index.ts
@@ -0,0 +1,11 @@
+export interface NewsResponse {
+    urlToImage: string;
+    author: string;
+    source: {
+        name: string;
+    };
+    publishedAt: string;
+    title: string;
+    description: string;
+    url: string;
+}
